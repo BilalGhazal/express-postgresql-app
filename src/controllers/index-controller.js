ba8f3e3bbc2a4bfae84e918f8ea632b85dca7b6f
bilal <bilalghazal98@gmail.com>
2025-08-11 03:38:08 +0300

    Set up the skeleton of the controller

---
+++ src/controllers/index-controller.js
@@ -0,0 +1,11 @@
+function getAllUsernames(req, res) {
+    console.log("usernames will be logged here - wip")
+}
+
+function createUsernameGet(req, res) {
+    
+}
+
+function createUsernamePost(req, res) {
+    console.log("username to be saved: ", req.body.username)
+}
